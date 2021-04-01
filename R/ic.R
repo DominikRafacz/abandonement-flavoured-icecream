93d1e0e70c9fada2be5f13a42e24a8192080d49d
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-04-01 14:29:55 +0200

    replaced 'at' with 'in'

--- R/ic.R
+++ R/ic.R
@@ -12,7 +12,7 @@ ic <- function(expr) {
     if (nchar(loc) == 0)
       loc <- "[filename unavailable]"
     cat(paste0("ic| ", loc,
-               if (is.null(parent_ref)) "" else paste0(" at ", as_label(parent_ref)), "\n"))
+               if (is.null(parent_ref)) "" else paste0(" in ", as_label(parent_ref)), "\n"))
   } else {
     value <- eval_tidy(argq)
     printed <- capture.output(print(value))
